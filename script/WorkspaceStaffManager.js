8672883a7cbe814c721f91fbfe2e04526574fe3b
lafhielzakaria <lafhielzakaria@gmail.com>
2025-11-17 14:52:48 +0100

    Complete the mobile version

---
+++ script/WorkspaceStaffManager.js
@@ -0,0 +1,9 @@
+// les fonctions : 
+// -function of renderStaffItem : fonction 4adi tbouvli 3la arrays ou 4adi tafficher kola 7aga fu plactha 7ta les modals tahoma!!!.
+// -function de supression .
+// -function de modifucation.
+// -function de openmodal et closemodal pour chaque modal.
+// -function d'ajoute .
+//-function de assign un staff :  
+
+     
